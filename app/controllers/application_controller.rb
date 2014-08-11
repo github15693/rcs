f0075019699590d42308bf64260c6d3fdd696e13
kt <khanh1111527@gmail.com>
2014-08-11 17:03:29 +0700

    template after log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :get_privileges
+  include ApplicationHelper
+  def get_privileges
+    @privileges = hash_to_object get_api('/privileges')
+  end
 end
--- test/models/public_function_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PublicFunctionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
