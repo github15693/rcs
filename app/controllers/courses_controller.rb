1beec21251747ef1a27ef452a9461ad0414a03fd
kt <khanh1111527@gmail.com>
2014-08-14 22:06:09 +0700

    fix module privileges

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,17 @@
 class CoursesController < ApplicationController
+  before_action :set_menu
+  include ApplicationHelper
+
+  def index
+    
+  end
+
+  def show
+    
+  end
+
+  private
+  def set_menu
+    session[:menu] = :courses
+  end
 end
