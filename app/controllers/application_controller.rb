45e18ead86c60d41bc8226de9f04eb30478e5ad3
Hung Nguyen <khanh1111527@gmail.com>
2014-08-14 13:54:04 +0700

    Change RootsController to higher level

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,35 +29,5 @@ class ApplicationController < ActionController::Base
     session[:token]= 'iMXnuxVicLRLRysLtjUN'
     session[:user_id] =14
   end
-
-  def rest_client url, http_usr = nil, http_pwd = nil
-    uri = api_domain + url
-    RestClient::Resource.new uri, http_usr, http_pwd
-  end
-
-  def api_get url, query_params = {}, http_usr = nil, http_pwd = nil
-    uri = url << api_params(query_params).to_s
-    JSON.parse rest_client(uri, http_usr, http_pwd).get, symbolize_names: true
-  end
-
-  def api_post url, query_params, http_usr = nil, http_pwd = nil
-    params = query_params#.to_json
-    JSON.parse rest_client(url, http_usr, http_pwd).post(params, content_type: "application/json"), symbolize_names: true
-  end
-
-  private
-    def api_domain
-      'http://rms.vn/api'
-    end
-
-    def api_params query_params = {}
-      queries = '?' unless query_params.empty?
-      key_last = query_params.keys.last
-      query_params.each do |key, value|
-        queries += key.to_s << '=' << value.to_s << ((key == key_last) ? '' : '&')
-        # queries += key.to_s << '=' << value.to_s << ('&' unless (key == key_last)).to_s
-      end
-      queries
-    end
 end
 
--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,4 +1,4 @@
-class WelcomesController < ApplicationController
+class WelcomesController < RootsController
   layout 'welcome'
   before_action :set_menu
 
