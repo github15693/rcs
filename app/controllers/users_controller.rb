6fac26b25151e63319e2d230df06e7f0aae124a5
Hung Nguyen <khanh1111527@gmail.com>
2014-08-18 11:10:35 +0700

    update session login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,17 @@ class UsersController < RootsController
   def create
     @user = api_post '/log_in', user_params
     if @user[:status] == 'success'
-      session[:user_id] = @user[:user_id]
-      session[:email] = @user[:email]
-      session[:name] = @user[:name]
-      session[:token] = @user[:token]
-      session[:condo_id] = @user[:condo_id]
+      session[:user_id] = user[:user_id]
+      session[:email] = user[:email]
+      session[:name] = user[:name]
+      session[:token] = user[:token]
+      session[:condo_id] = user[:condo_id]
       if user_params[:remember]
-        cookies[:user_id] = @user[:user_id]
-        cookies[:email] = @user[:email]
-        cookies[:name] = @user[:name]
-        cookies[:token] = @user[:token]
-        cookies[:condo_id] = @user[:condo_id]
+        cookies[:user_id] = user[:user_id]
+        cookies[:email] = user[:email]
+        cookies[:name] = user[:name]
+        cookies[:token] = user[:token]
+        cookies[:condo_id] = user[:condo_id]
       end
     end
   end
@@ -20,6 +20,10 @@ class UsersController < RootsController
   private
     def user_params
       params.require(:user).permit(:email, :password, :remember)
+    end
+
+    def user
+      @user[:data]
     end
 end
 
